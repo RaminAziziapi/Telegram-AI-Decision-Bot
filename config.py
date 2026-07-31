9591c545472f5645f154a6466df7ada2e23ced94
RaminAziziapi <arshejxm@gmail.com>
2026-08-01 00:54:30 +0430

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,10 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
+ADMIN_IDS = [
+    int(x)
+    for x in os.getenv("ADMIN_IDS").split(",")
+]
 
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 WEBHOOK_PATH = os.getenv("WEBHOOK_PATH", "webhook")
