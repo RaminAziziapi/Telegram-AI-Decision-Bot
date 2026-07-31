4986da05df209d44809e990c748c1318d4407982
RaminAziziapi <arshejxm@gmail.com>
2026-08-01 00:50:12 +0430

    Create ai_api.py

---
+++ ai_api.py
@@ -0,0 +1,31 @@
+import google.generativeai as genai
+
+from config import GEMINI_API_KEY
+
+
+genai.configure(
+    api_key=GEMINI_API_KEY
+)
+
+
+model = genai.GenerativeModel(
+    "gemini-2.5-flash"
+)
+
+
+def ask_ai(messages):
+
+    conversation = ""
+
+    for msg in messages:
+        role = msg["role"]
+        content = msg["content"]
+
+        conversation += f"{role}: {content}\n"
+
+
+    response = model.generate_content(
+        conversation
+    )
+
+    return response.text
