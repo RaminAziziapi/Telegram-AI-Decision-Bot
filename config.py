f82f3dc951e2519c5477ade21974c6fa0b43f26d
RaminAziziapi <arshejxm@gmail.com>
2026-07-31 23:29:54 +0430

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,7 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
+
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+WEBHOOK_PATH = os.getenv("WEBHOOK_PATH", "webhook")
+PORT = int(os.getenv("PORT", 8080))
